c4c907a72df4bf059a4ea280cc27b8970e7eb9a3
cheginit <taher.chegini@gmail.com>
2019-09-30 13:11:30 -0500

    added support for polybar on multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,29 @@
-#!/usr/bin/env sh
+#!/bin/bash
+(
+  flock 200
 
-## Add this to your wm startup file.
+  killall -q polybar
 
-# Terminate already running bar instances
-killall -q polybar
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+  for m in $outputs; do
+    if [[ $m == "HDMI1" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar -c ~/.config/polybar/config.ini -r main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
+
